eda7e60f7219f871979a06864f9853a807d499df
Mind <57893196+MindXL@users.noreply.github.com>
2022-12-29 20:40:39 +0800

    Added missing files to premake file.

--- premake5.lua
+++ premake5.lua
@@ -14,6 +14,13 @@ project "GLFW"
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
+
+		"src/null_init.c",
+		"src/null_joystick.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+
+		"src/platform.c",
 		"src/vulkan.c",
 		"src/window.c"
 	}
@@ -26,6 +33,7 @@ project "GLFW"
 		{
 			"src/win32_init.c",
 			"src/win32_joystick.c",
+			"src/win32_module.c",
 			"src/win32_monitor.c",
 			"src/win32_time.c",
 			"src/win32_thread.c",
